18192263920cff230dc1e08b2f1caf2546847a76
MalstromRD <118015429+RMalstrom@users.noreply.github.com>
2024-09-26 14:04:37 -0400

    Create problem5BareMinimum.py
    Added the shortest and simplest solution i will attempt

---
+++ problem5BareMinimum.py
@@ -0,0 +1,11 @@
+"""
+Richard Malstrom
+CMSC-1380
+
+This program is the bare minimum needed to solve Problem 5 on Page 103.
+
+Date last modified: 9.26.2024
+"""
+
+userTemp = int(input("Please enter the temperature you would like to convert to Celsius: "))
+print(f'Your converted temperature is {(5 / 9) * (userTemp - 32)} Celsius')
