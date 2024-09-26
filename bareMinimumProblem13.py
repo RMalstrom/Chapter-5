4cf998a84f3e5a2803f85ffee652ae7c1203f08d
MalstromRD <118015429+RMalstrom@users.noreply.github.com>
2024-09-26 14:06:36 -0400

    Create bareMinimumProblem13.py
    Added the smallest possible solution to problem 13

---
+++ bareMinimumProblem13.py
@@ -0,0 +1,11 @@
+"""
+Richard Malstrom
+CMSC-1380
+
+This program is the bare minimum needed to solve Problem 13 on Page 103.
+
+Date last modified: 9.26.2024
+"""
+import random
+
+print(f'Your first dice is: {random.randint(1,6)} and your second dice is: {random.randint(1,6)}.')
