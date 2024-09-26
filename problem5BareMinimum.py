8fbd150660d70cf34de2ad4ccca1c376a4b6ff6f
MalstromRD <118015429+RMalstrom@users.noreply.github.com>
2024-09-26 14:10:01 -0400

    Update problem5BareMinimum.py
    Added slight commenting

--- problem5BareMinimum.py
+++ problem5BareMinimum.py
@@ -2,10 +2,10 @@
 Richard Malstrom
 CMSC-1380
 
-This program is the bare minimum needed to solve Problem 5 on Page 103.
+This program is the bare minimum needed to solve Problem 13 on Page 103.
 
 Date last modified: 9.26.2024
 """
+import random # Import the library to generate random numbers
 
-userTemp = int(input("Please enter the temperature you would like to convert to Celsius: "))
-print(f'Your converted temperature is {(5 / 9) * (userTemp - 32)} Celsius')
+print(f'Your first dice is: {random.randint(1,6)} and your second dice is: {random.randint(1,6)}.')
